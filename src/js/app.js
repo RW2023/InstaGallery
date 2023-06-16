3356bfb11569df647aaff196ce80cbe8646d7a66
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-06-16 15:27:19 -0400

    add scroll reveal

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
 
+ScrollReveal().reveal('#me', {
+    distance: '100%',
+    origin: 'right',
+    duration: 1000,
+    // delay: 300,
+    // reset: true
+});
 
+ScrollReveal().reveal('.photos img', {
+    distance: '400px',
+    origin: 'top',
+    duration: 1000,
+    delay: 30,
+    // reset: true
+});
