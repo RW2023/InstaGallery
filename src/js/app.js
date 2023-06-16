380f44bbde891a4d0c41ed328aec3236a6f9f2df
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-06-16 15:57:18 -0400

    add animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,28 @@
 
-ScrollReveal().reveal('#me', {
-    distance: '100%',
-    origin: 'right',
-    duration: 1000,
-    // delay: 300,
-    // reset: true
+anime({
+    targets: '#me',  // Target the img element with the id of 'me'
+    scale: [0.1, 1],  // Start small and grow to full size
+    duration: 2000,   // Duration of 2000ms, or 2 seconds
+    easing: 'easeOutCubic' // Speed curve for the animation
 });
 
-ScrollReveal().reveal('.photos img', {
-    distance: '400px',
-    origin: 'top',
-    duration: 1000,
-    delay: 30,
-    // reset: true
+anime.timeline({
+    targets: '.photos img',
+    easing: 'easeOutElastic(1, .8)',  // Easing function for bounce effect
+    delay: anime.stagger(200)
+})
+    .add({
+        translateY: ['-100%', '0%'],
+    })
+    .add({
+        scale: [1, 1.1, 1]
+    });
+
+anime({
+    targets: '.fa-hand',
+    rotate: [{ value: '-25deg' }, { value: '15deg' }, { value: '-15deg' }, { value: '0deg' }],
+    duration: 700,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
 });
